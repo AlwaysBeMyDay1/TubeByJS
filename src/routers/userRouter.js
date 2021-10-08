27fc9dbf1519b357b4aefd2cf47a19e714e86f3d
Daeyeon Kim <eodus09011@naver.com>
2021-10-09 02:13:36 +0900

    b4 cloning

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {edit, remove} from "../Controllers/userController";
+import {edit, see, logout, remove } from "../Controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
+userRouter.get("/logout", logout);
 userRouter.get("/remove", remove);
+userRouter.get("/:id", see);
 
 export default userRouter;
