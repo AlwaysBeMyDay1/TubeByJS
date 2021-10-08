dc2270c96c3ba14d8778676b0daac2961458b0ad
Daeyeon Kim <eodus09011@naver.com>
2021-10-09 00:44:59 +0900

    settings finished

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+const PORT = 4000;
+
+const app = express();
+//app 설정 (ex. get request에 응답하는 법,,)
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = ()=> console.log(`Server listening on port http://localhost:${PORT}`);
+app.listen(PORT, handleListening);
+// 위 코드와 동일// app.listen(4000, ()=> console.log("Server listening on port 4000"));
