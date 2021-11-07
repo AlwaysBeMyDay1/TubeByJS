242d17b7fcc67fa9866d80a4044cff1cb25b9756
Daeyeon Kim <eodus09011@naver.com>
2021-11-07 18:00:05 +0900

    WEBPACK

--- src/server.js
+++ src/server.js
@@ -30,7 +30,8 @@ app.use(
 //(들어온 사람에게 텍스트(쿠키)를 주고, 해당 텍스트로 유저가 누구인지 알아냄)
 
 app.use(localsMiddleware); //← 이 미들웨어로는 session object에 접근
-app.use("/uploads", express.static("uploads"))
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/js/main.js",
+  mode:"development",
+  watch:true,
+  plugins:[new MiniCssExtractPlugin({
+    filename : "css/style.css"
+  })],
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean:true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader", //js코드를 babel-loader라는 loader로 가공
+          options: { //그 babel loader에 몇몇 option 추가
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },{
+        test : /\.scss$/,
+        use:[MiniCssExtractPlugin.loader,"css-loader", "sass-loader"],
+      }
+    ],
+  },
+};
