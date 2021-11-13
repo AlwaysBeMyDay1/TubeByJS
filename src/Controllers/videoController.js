c223fd0e92305df17a3278a3c1d8e2b42118066b
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2021-11-13 18:11:33 +0900

    CSS finished

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -6,7 +6,9 @@ import Video from "../models/Video";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "asc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "asc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -114,7 +116,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(`${keyword}$`, "i"),
       },
-    });
+    }).populate("owner");
   }
   return res.render("videos/search", { pageTitle: "Search", videos });
 };
